faf5325f8f3e770ce7787ee3740fc0992ea86082
shawn6687 <112918547+shawn6687@users.noreply.github.com>
2022-09-06 11:30:39 +0800

    Add files via upload

---
+++ week01-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("我是林逸庠\n");
+    ///有時候中文會有問題可以用英文
+	//註解 comments 是給人看得
+	///CodeBlocks 如果畫三條，會比較深 
+
+}
