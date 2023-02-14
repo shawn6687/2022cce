2a77a7972ec5f8419257416d5ca6e8d588ad5f0b
shawn6687 <shawnlin6687@gmail.com>
2023-02-14 11:57:23 +0800

    week01

--- week01/week01-2.cpp
+++ week01/week01-2.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
 int main()
 {
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("我是林逸庠\n");
-    ///有時候中文會有問題可以用英文
-	//註解 comments 是給人看得
-	///CodeBlocks 如果畫三條，會比較深 
+    printf("請輸入1個字母: ");
+    char c;
+    scanf("%c", &c);
+    printf("你輸入了 %c \n", c);
 
+    printf("請輸入1個整數: ");
+    int a;
+    scanf("%d", &a);
+    printf("你輸入了 %d \n", a);
+    return 0;
 }
---
+++ week01/week01_1/week01_1.pde
@@ -0,0 +1,17 @@
+size(400,400);
+fill(255,0,0);
+noStroke();
+//ellipse(50,50,100,100);
+for(int y=0;y<400;y++)
+{
+   for(int x=0;x<400;x++)
+   {
+     float x2=(x-200)/100.0;
+     float y2=(200-y)/100.0;
+     float left= x2*x2 + y2*y2-1;
+     if(left*left*left - x2*x2*y2*y2*y2 < 0)
+     {
+       ellipse(x,y,2,2);
+     }
+   }
+}
