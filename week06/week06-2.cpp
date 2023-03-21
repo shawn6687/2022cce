1177143d83bdfdee02f97f4f8a20ad9adcb979c4
shawn6687 <shawnlin6687@gmail.com>
2023-03-21 12:13:16 +0800

    week06

---
+++ week06/week06-2.cpp
@@ -0,0 +1,13 @@
+
+    bool isPalindrome(int x) {
+     int x2=x;
+     int r=0;
+     while(x>0)
+     {
+         r=r*10+x%10;
+         x=x/10;
+
+     }
+     if(x2==r)return true;
+     else return false;
+    }
